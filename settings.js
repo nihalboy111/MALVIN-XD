d071112ca7f26ec21649887a5fbcd269525d0a03
nihalboy111 <nihalboy111@gmail.com>
2025-02-21 03:58:17 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "MALVIN-XD~h5ZWiC4D#LPycncAljGrZqy7_vFP27jNZC_210TlVfqnb0PeyC9Y",
 // add your Session Id 
 
 PREFIX: process.env.PREFIX || ".",
